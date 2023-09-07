8e0ec123095381f150d33a61533d31c293969c5b
Corey <crk00008@mix.wvu.edu>
2023-09-07 12:39:13 -0400

    [Rehoboam] Version 1.3.8

--- rehoboam/rehoboam.py
+++ rehoboam/rehoboam.py
@@ -849,7 +849,7 @@ class Rehoboam(commands.Cog):
                     except:
                         logger.info(f"Could not modify start time for event {after.id}")
 
-    @tasks.loop(seconds=5)
+    @tasks.loop(seconds=10)
     async def check_events(self):
         global to_remove
         global to_alert
